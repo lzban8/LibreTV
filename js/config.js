0d7f3ed98c3c50e9db6c2daf3871dc925b31ab72
lzban8 <41175193+lzban8@users.noreply.github.com>
2025-04-07 10:43:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -25,6 +25,11 @@ const API_SITES = {
         name: '非凡影视',
         detail: 'http://ffzy5.tv',
     },
+    gying: {
+        api: 'https://www.gying.net',
+        name: '观影',
+        detail: 'https://www.gying.net',
+    },
     
 };
 
